a164d967ae769ec8fcc86a47ec38e18c7688eaa7
MeesterThomas <98034828+MeesterThomas@users.noreply.github.com>
2025-10-07 14:44:24 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ async function onCharacterSelectButton(ID)
     if (startBattle == true)
         return;
     let characterStats = await fetch("../txt/CharacterStats.txt");
-    document.getElementById("battleNameL").innerHTML = characterStats;
+    document.getElementById("battleNameL").innerHTML = characterStats.text();
 }
 function onBattleStartButton()
 {
